6d4910b8b9780e3fc1f0573badd7423d7bf212c9
joaomm <joaomm88@gmail.com>
2010-09-21 19:45:26 -0300

    Provavelmente algoritmo de uniao completo

--- silhueta.erl
+++ silhueta.erl
@@ -5,21 +5,21 @@ silhueta_de_edificio({Esq, Alt, Dir}) ->
 	[{Esq,Alt}, {Dir,0}].
 
 uniao(ListaDePares1, ListaDePares2) ->
-	uniao_recursiva(ListaDePares1, ListaDePares2, []).
+	uniao_recursiva(ListaDePares1, ListaDePares2, [], 0).
 
-uniao_recursiva([], ListaDePares2, Resultado) ->
+uniao_recursiva([], ListaDePares2, Resultado, _) ->
 	lists:reverse(Resultado) ++ ListaDePares2;
 
-uniao_recursiva(ListaDePares1, [], Resultado) ->
+uniao_recursiva(ListaDePares1, [], Resultado, _) ->
 	lists:reverse(Resultado) ++ ListaDePares1;
 	
-uniao_recursiva(ListaDePares1, ListaDePares2, Resultado) ->
-	[Primeiro1 | _Resto1] = ListaDePares1,
-	[Primeiro2 | Resto2] = ListaDePares2,
+uniao_recursiva(ListaDePares1, ListaDePares2, Resultado, UltimaAlturaSilhuetaOposta) ->
+	[Primeiro1 | _] = ListaDePares1,
+	[Primeiro2 | _] = ListaDePares2,
     case compara(Primeiro1, Primeiro2) of
-		x_do_primeiro_mais_a_esquerda -> verifica_altura_e_chama_uniao(ListaDePares1, ListaDePares2, Resultado);
-		x_do_segundo_mais_a_esquerda -> uniao_recursiva(ListaDePares1, Resto2, [Primeiro2 | Resultado]);
-		x_igual -> escolhe_maior_altura_e_chama_uniao(ListaDePares1, ListaDePares2, Resultado)
+		x_do_primeiro_mais_a_esquerda -> verifica_altura_e_chama_uniao(ListaDePares1, ListaDePares2, Resultado, UltimaAlturaSilhuetaOposta);
+		x_do_segundo_mais_a_esquerda -> verifica_altura_ultima_silhueta_e_chama_uniao(ListaDePares1, ListaDePares2, Resultado, UltimaAlturaSilhuetaOposta);
+		x_igual -> escolhe_maior_altura_e_chama_uniao(ListaDePares1, ListaDePares2, Resultado, UltimaAlturaSilhuetaOposta)
 	end.
 
 compara({X1, _Alt1}, {X2, _Alt2}) when X1 < X2 ->
@@ -31,37 +31,45 @@ compara({X1, _Alt1}, {X2, _Alt2}) when X1 > X2 ->
 compara({X1, _Alt1}, {X1, _Alt2}) ->
 	x_igual.
 
-verifica_altura_e_chama_uniao(ListaDePares1, ListaDePares2, []) ->
+verifica_altura_e_chama_uniao(ListaDePares1, ListaDePares2, [], _) ->
 	[Primeiro1 | Resto1] = ListaDePares1,
-	uniao_recursiva(ListaDePares2, Resto1, [Primeiro1]);
+	uniao_recursiva(ListaDePares2, Resto1, [Primeiro1], 0);
 	
-verifica_altura_e_chama_uniao(ListaDePares1, ListaDePares2, Resultado) ->
+verifica_altura_e_chama_uniao(ListaDePares1, ListaDePares2, Resultado, _) ->
 	[Primeiro1 | Resto1] = ListaDePares1,
 	{_, Altura1} = Primeiro1,
 	[{_, AlturaUltimo} | _ ] = Resultado,	
 	case AlturaUltimo < Altura1 of
-		true -> uniao_recursiva(ListaDePares2, Resto1, [Primeiro1 | Resultado]);
-		false -> uniao_recursiva(Resto1, ListaDePares2, Resultado)
+		true -> uniao_recursiva(ListaDePares2, Resto1, [Primeiro1 | Resultado], AlturaUltimo);
+		false -> uniao_recursiva(Resto1, ListaDePares2, Resultado, Altura1)
 	end.
-
-escolhe_maior_altura_e_chama_uniao(ListaDePares1, ListaDePares2, []) ->
+	
+verifica_altura_ultima_silhueta_e_chama_uniao(ListaDePares1, ListaDePares2, Resultado, UltimaAlturaSilhuetaOposta) ->
+	[Elemento | Resto2] = ListaDePares2,
+	{X, Alt} = Elemento,
+	case UltimaAlturaSilhuetaOposta < Alt of
+		true -> uniao_recursiva(ListaDePares1, Resto2, [Elemento | Resultado], UltimaAlturaSilhuetaOposta);
+		false -> uniao_recursiva(Resto2, ListaDePares1, [{X, UltimaAlturaSilhuetaOposta} | Resultado], Alt)
+	end.
+	
+escolhe_maior_altura_e_chama_uniao(ListaDePares1, ListaDePares2, [], _) ->
 	{ListaComElementoMaiorAltura, ListaComMenor} = lista_com_maior_altura(ListaDePares1, ListaDePares2),
 	[ElementoComAlturaMaior | RestoComMaiorAltura] = ListaComElementoMaiorAltura,
-	[_ | RestoComMenorAltura] = ListaComMenor,
-	uniao_recursiva(RestoComMenorAltura, RestoComMaiorAltura, [ElementoComAlturaMaior]);
+	[{_,AlturaMenor} | RestoComMenorAltura] = ListaComMenor,
+	uniao_recursiva(RestoComMenorAltura, RestoComMaiorAltura, [ElementoComAlturaMaior], AlturaMenor);
 	
-escolhe_maior_altura_e_chama_uniao(ListaDePares1, ListaDePares2, Resultado) ->
+escolhe_maior_altura_e_chama_uniao(ListaDePares1, ListaDePares2, Resultado, _) ->
 	[_| Resto1] = ListaDePares1,
 	[_| Resto2] = ListaDePares2,
 	
 	[{_, AlturaUltimo} | _ ] = Resultado,
 	{ListaComElementoMaiorAltura, ListaComMenor} = lista_com_maior_altura(ListaDePares1, ListaDePares2),
 	[ElementoComAlturaMaior | RestoComMaiorAltura] = ListaComElementoMaiorAltura,
-	[_ | RestoComMenorAltura] = ListaComMenor,
+	[{_, AlturaMenor} | RestoComMenorAltura] = ListaComMenor,
 	{_, AlturaDoElementoMaior} = ElementoComAlturaMaior, 
 	case AlturaDoElementoMaior == AlturaUltimo of
-		true -> uniao_recursiva(Resto1, Resto2, Resultado);
-		false -> uniao_recursiva(RestoComMenorAltura, RestoComMaiorAltura, [ElementoComAlturaMaior | Resultado])
+		true -> uniao_recursiva(Resto1, Resto2, Resultado, 0);
+		false -> uniao_recursiva(RestoComMenorAltura, RestoComMaiorAltura, [ElementoComAlturaMaior | Resultado], AlturaMenor)
 	end.
 	
 lista_com_maior_altura(ListaDePares1, ListaDePares2) ->
