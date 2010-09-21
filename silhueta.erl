d336aef91d7561be62f277dcfb6f69d62dea9bf4
joaomm <joaomm88@gmail.com>
2010-09-21 16:13:39 -0300

    Casos anteriores invertidos

--- silhueta.erl
+++ silhueta.erl
@@ -9,29 +9,43 @@ uniao(ListaDePares1, ListaDePares2) ->
 
 uniao_recursiva([], ListaDePares2, Resultado) ->
 	lists:reverse(Resultado) ++ ListaDePares2;
+
+uniao_recursiva(ListaDePares1, [], Resultado) ->
+	lists:reverse(Resultado) ++ ListaDePares1;
 	
 uniao_recursiva(ListaDePares1, ListaDePares2, Resultado) ->
 	[Primeiro1 | Resto1] = ListaDePares1,
-	[Primeiro2 | _Resto2] = ListaDePares2,
+	[Primeiro2 | Resto2] = ListaDePares2,
     case compara(Primeiro1, Primeiro2) of
 		x_do_primeiro_mais_a_esquerda -> uniao_recursiva(Resto1, ListaDePares2, [Primeiro1 | Resultado]);
+		x_do_segundo_mais_a_esquerda -> uniao_recursiva(ListaDePares1, Resto2, [Primeiro2 | Resultado]);
 		x_igual -> escolhe_maior_altura_e_chama_uniao(ListaDePares1, ListaDePares2, Resultado)
 	end.
 
 compara({X1, _Alt1}, {X2, _Alt2}) when X1 < X2 ->
 	x_do_primeiro_mais_a_esquerda;
 
+compara({X1, _Alt1}, {X2, _Alt2}) when X1 > X2 ->
+	x_do_segundo_mais_a_esquerda;
+
 compara({X1, _Alt1}, {X1, _Alt2}) ->
 	x_igual.
 
 escolhe_maior_altura_e_chama_uniao(ListaDePares1, ListaDePares2, Resultado) ->
-	[_Primeiro1 | Resto1] = ListaDePares1,
+	[Primeiro1 | Resto1] = ListaDePares1,
 	[Primeiro2 | Resto2] = ListaDePares2,
-	{_, Altura2} = Primeiro2,
 	[{_, AlturaUltimo} | _ ] = Resultado,
-	case Altura2 == AlturaUltimo of
+	ElementoComAlturaMaior = elemento_com_maior_altura(Primeiro1, Primeiro2),
+	{_, AlturaDoElementoMaior} = ElementoComAlturaMaior, 
+	case AlturaDoElementoMaior == AlturaUltimo of
 		true -> uniao_recursiva(Resto1, Resto2, Resultado);
-		false -> uniao_recursiva(Resto1, Resto2, [Primeiro2 | Resultado])
+		false -> uniao_recursiva(Resto1, Resto2, [ElementoComAlturaMaior | Resultado])
 	end.
 	
-	
+elemento_com_maior_altura(Elemento1, Elemento2) ->
+	{_, Alt1} = Elemento1, 
+	{_, Alt2} = Elemento2,
+	case Alt1 < Alt2 of
+		true -> Elemento2;
+		false -> Elemento1
+	end.
--- silhueta_test.erl
+++ silhueta_test.erl
@@ -16,17 +16,23 @@ uniao_nao_grudados_test_() ->
 		?_assert(silhueta:uniao([{0, 1},{1, 0}], [{3, 1}, {4, 0}]) == [{0, 1}, {1, 0}, {3, 1}, {4, 0}])
 	].
 
-uniao_grudados_e_mesma_altura_test_() ->
+uniao_grudados_e_mesma_altura_test() ->
 	[
-		?_assert(silhueta:uniao([{0, 1},{1, 0}], [{1, 1}, {2, 0}]) == [{0, 1}, {2, 0}]),
-		?_assert(silhueta:uniao([{0, 2},{1, 0}], [{1, 2}, {2, 0}]) == [{0, 2}, {2, 0}]),
-		?_assert(silhueta:uniao([{0, 2},{1, 0}], [{1, 2}, {3, 0}]) == [{0, 2}, {3, 0}]),
-		?_assert(silhueta:uniao([{1, 2},{2, 0}], [{2, 2}, {3, 0}]) == [{1, 2}, {3, 0}]),
-		?_assert(silhueta:uniao([{0, 2},{2, 0}], [{2, 2}, {3, 0}]) == [{0, 2}, {3, 0}])		
+		?assertEqual(silhueta:uniao([{0, 1},{1, 0}], [{1, 1}, {2, 0}]), [{0, 1}, {2, 0}]),
+		?assertEqual(silhueta:uniao([{0, 2},{1, 0}], [{1, 2}, {2, 0}]), [{0, 2}, {2, 0}]),
+		?assertEqual(silhueta:uniao([{0, 2},{1, 0}], [{1, 2}, {3, 0}]), [{0, 2}, {3, 0}]),
+		?assertEqual(silhueta:uniao([{1, 2},{2, 0}], [{2, 2}, {3, 0}]), [{1, 2}, {3, 0}]),
+		?assertEqual(silhueta:uniao([{0, 2},{2, 0}], [{2, 2}, {3, 0}]), [{0, 2}, {3, 0}]),
+		?assertEqual(silhueta:uniao([{2, 2}, {3, 0}], [{0, 2},{2, 0}]), [{0, 2}, {3, 0}])
 	].
 
 uniao_grudados_com_alturas_diferentes_test() ->
 	[
 		?assertEqual(silhueta:uniao([{0, 1}, {1, 0}], [{1, 2}, {2, 0}]), [{0, 1}, {1, 2}, {2, 0}]),
 		?assertEqual(silhueta:uniao([{0, 3}, {1, 0}], [{1, 2}, {2, 0}]), [{0, 3}, {1, 2}, {2, 0}])
+	].
+
+uniao_com_sobreposicao_total_test() ->
+	[
+		?assertEqual(silhueta:uniao([{0, 2}, {3, 0}], [{1, 1}, {2, 0}]), [{0, 2}, {3, 0}])
 	].
